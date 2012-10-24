1cfcffd19ea0a6d875eac689adc6d7a40076fbac
Jim Newsome <jim@jimnewsome.net>
2012-10-24 01:30:43 -0400

    assign to 'contentWidth' instead of undefined/unused contentlwidth

--- rapanui-sdk/RNFactory.lua
+++ rapanui-sdk/RNFactory.lua
@@ -78,7 +78,7 @@ function RNFactory.init()
     RNFactory.width = lwidth
     RNFactory.height = lheight
 
-    contentlwidth = lwidth
+    contentWidth = lwidth
     contentHeight = lheight
 
     RNFactory.outWidth = RNFactory.width
